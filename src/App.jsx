558f93e084500580bab79011df651b92cd5869cf
vishwesh <vishwesh103@gmail.com>
2023-11-01 00:49:45 -0700

    App/MainView: Add dark mode and main view.

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,40 @@
-import { useState } from 'react'
+// import { useState } from 'react'
 import './App.css'
-import Example from './components/Example';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import MainView from './components/MainView';
+
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
+
 
 function App() {
 
-  const [count, setCount] = useState(1);
+  // const [count, setCount] = useState(1);
 
-  let data = [{},{},{}];
+  // let data = [{},{},{}];
 
   return (
     <div>
-      <div className='parent-container'>
-        <div className='graph-heading'>
-          <h4 className='graph-heading'>Template Graph</h4>
-        </div>
-        <div className='graph-heading'>
-          <h4>
-            <button id='candidate-control' disabled={count===1} onClick={()=>setCount(count-1)}>{'<'}</button>
-            Candidate Graph {count}
-            <button id='candidate-control' disabled={count===4} onClick={()=>setCount(count+1)}>{'>'}</button>
-          </h4>
-        </div>
-      </div>
-      <div className='parent-container'>
-        <div className='container'>
-          <Example data={data}/>
-        </div>
-        <div className='container'></div>
-      </div>
+      <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <Box sx={{ flexGrow: 1, mx:"1em", my:"2em" }}>
+        <Grid container spacing={2}>
+          <Grid item lg={6}>
+           <MainView />
+          </Grid>
+          <Grid item lg={6}>
+            <MainView />
+          </Grid>
+        </Grid>
+      </Box>
+    </ThemeProvider>
     </div>
       
   )
