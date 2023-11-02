ba90c7d0ffd6b50024c6f39b070143030bd94a51
Parth Shah <parthshah576@gmail.com>
2023-11-02 10:33:11 -0700

    src/App.jsx: parse template graph and create filter object

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import { useState, useEffect } from 'react'
-import { csvParse } from 'd3-dsv';
+import * as d3 from 'd3';
 import './App.css'
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
 import MainView from './components/MainView';
+import { graphFilepaths } from './constants.js';
 
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
@@ -14,10 +15,25 @@ const darkTheme = createTheme({
   },
 });
 
+/*
+Custom edge types:
+0 = email
+1 = phone
+2 = buy/sell
+3 = travel
+*/
+
 function App() {
 
   const [count, setCount] = useState(1);
-  const [templateData, setTemplateData] = useState(null);
+  const [dataStore, setDataStore] = useState({
+    'template': null,
+    //'candidate1': null,
+    //'candidate2': null,
+    //'candidate3': null,
+    //'candidate4': null,
+    //'candidate5': null,
+  });
   const [filterObj, setFilterObj] = useState({
     'startTime': null,
     'endTime': null,
@@ -25,8 +41,30 @@ function App() {
     'selectedNode':null
   })
 
+  const startDate = new Date('2025-01-01T00:00:00');
+
   useEffect(()=>{
-    csvParse('/data/processed/CGCS-Template-Processed-data')
+      try {
+        Object.keys(dataStore).forEach(async (graphName) => {
+            const data = await d3.csv(graphFilepaths[graphName], (d) => {
+              return {
+                'source': d.Source,
+                'target': d.Target,
+                'eType': d.eType,
+                'weight': d.Weight,
+                'time': new Date(startDate.getTime() + (d.Time*1000)),
+              };
+            });
+            if (data) {
+              setDataStore({
+                ...dataStore,
+                [graphName]: data
+              });
+            }
+        })
+      } catch(error) {
+        console.log(error);
+      }
   },[]);
 
   return (
