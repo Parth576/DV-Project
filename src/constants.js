b5cd69c565b8e2dd8f56505520e1b0fce9d62662
Parth Shah <parthshah576@gmail.com>
2023-11-02 10:33:11 -0700

    src/App.jsx: parse template graph and create filter object

---
+++ src/constants.js
@@ -0,0 +1,10 @@
+export const graphFilepaths = {
+  'template': '../data/processed/CGCS-Template-Processed-data.csv',
+  'candidate1': '',
+  'candidate2': '',
+  'candidate3': '',
+  'candidate4': '',
+  'candidate5': '',
+}
+
+
