549b92aabc18d214874e65382b2bd13eb3db687c
Yukta Sarode <ysarode@EN4145749-ETS.local>
2023-11-22 17:28:32 -0700

    JS and CSS folders added.

---
+++ js/main.js
@@ -0,0 +1,45 @@
+const startDate = new Date('2025-01-01T00:00:00');
+
+async function convertData(path){
+    const newData = await d3.csv(path, (d)=> {
+
+        return{
+            
+            'source': d.Source,
+            'target': d.Target,
+            'eType': d.eType,
+            'weight': d.Weight,
+            'time': new Date(startDate.getTime() + (d.Time*1000)),
+
+
+
+          
+
+        }
+    })
+    return newData;
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    // Hint: create or set your svg element inside this function
+ 
+    // This will load your two CSV files and store them into two arrays.
+    Promise.all([convertData('data/processed/CGCS-Template-Processed-data.csv')])
+         .then(function (values) {
+             console.log('loaded Data');
+             data = values[0];
+
+             console.log(data)
+            
+ 
+             // Hint: This is a good spot for doing data wrangling
+ 
+ 
+             drawNetworkChart();
+         });
+ });
+ 
+ // Use this function to draw the network chart.
+ function  drawNetworkChart() {
+     console.log('trace:drawNetworkpChart()');
+ }
