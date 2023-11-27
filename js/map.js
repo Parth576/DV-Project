f9bd4d4089f99f7dfcee819547b0aa1b73ae4203
Yukta Sarode <ysarode@EN4145749-ETS.local>
2023-11-27 16:51:17 -0700

    Map done with real data. Clearing remaining.

--- js/map.js
+++ js/map.js
@@ -14,23 +14,61 @@ function drawMapChart() {
 
     const dataStore = appState.getDataStore();
     // console.log(dataStore)
-    
 
-    var leftMapData=dataStore.leftGraph;
-    var rightMapData=dataStore.rightGraph;
+    var filtersOnMain=appState.getFilters();
+    console.log(filtersOnMain);
 
 
-    var filteredLeft = leftMapData.filter(data => data.eType === 3);
-    var filteredRight = rightMapData.filter(data => data.eType === 3);
-    console.log(filteredLeft)
-    console.log(filteredRight)
+    if(filtersOnMain.leftGraph==="template" || filtersOnMain.rightGraph==="template"){
+        console.log("template found", filtersOnMain.leftGraph, filtersOnMain.rightGraph)
+        console.log(dataStore)
 
+        if(filtersOnMain.leftGraph === "template"){
+            var tempColor="#704F4F";
+            var pointColor="#F1A661";
+            var pointsGraph=dataStore.rightGraph
+        }
+        else{
+            var tempColor="#F1A661";
+            var pointColor="#704F4F";
+            var pointsGraph=dataStore.leftGraph
+           
+        }
+       console.log(pointsGraph)
+        
+
+
+        var opacityData = [
+            { targetLocation: 3.0, count: 13 },
+            { targetLocation: 4.0, count: 10 },
+            { targetLocation: 0.0, count: 9 },
+            { targetLocation: 1.0, count: 7 },
+            { targetLocation: 2.0, count: 7 },
+            { targetLocation: 5.0, count: 6 }
+        ];
+
+        var pointsGraph2 = pointsGraph.filter(data => data.eType === 3);
+        
+        drawMapNewChart(opacityData,pointsGraph2,tempColor,pointColor);
+
+    }
+    else{
+        var pointsLeftData=dataStore.leftGraph;
+        var pointsRightData = dataStore.rightGraph;
+        drawMapNormalChart(pointsLeftData,pointsRightData);
+
+    }
+
+}
+
+function drawMapNewChart(data,leftData,tempColor,pointColor){
     const svg = d3.select("#myMap svg");
     const width = vw(43.5);
     const height = vh(50);
 
     svg.attr("width", width)
    .attr("height", height);
+   console.log(data,leftData)
    
 
     // Map and projection
@@ -39,16 +77,6 @@ function drawMapChart() {
         .translate([width / 2, height / 2]);
 
        
-
-        const data = [
-            { targetLocation: 5, count: 30 },
-            { targetLocation: 0, count: 30 },
-            { targetLocation: 4, count: 25 },
-            { targetLocation: 1, count: 10 },
-            { targetLocation: 3, count: 10 },
-            { targetLocation: 2, count: 5 }
-        ];
-        
         const countryIntMap = {
             0: 'USA',
             1: 'Italy',
@@ -58,13 +86,7 @@ function drawMapChart() {
             5: 'Canada',
         };
 
-        const leftData = [
-            { lat: 28.7, long: 56.9 },
-            { lat: 25.7, long: 96.9 },
-            { lat: 18.7, long: 6.9 }
-        ];
-
-
+       
     const opacityScale = d3.scaleLinear()
         .domain([0, d3.max(data, d => d.count)])
         .range([0.3, 1]);
@@ -82,7 +104,7 @@ function drawMapChart() {
         .attr("fill", d => {
             const country = d.properties.name;
             const countryData = data.find(item => countryIntMap[item.targetLocation] === country);
-            return countryData ? "#3876BF" : '#c0c1c2'; // Grey for countries not in the data
+            return countryData ? tempColor : '#c0c1c2'; // Grey for countries not in the data
         })
         .style("opacity", d => {
             const country = d.properties.name;
@@ -112,14 +134,67 @@ function drawMapChart() {
         .data(leftData)
         .enter()
         .append("circle")
-        .attr("cx", d => projection([d.long, d.lat])[0]) // Use the projection to convert lat/long to x/y
-        .attr("cy", d => projection([d.long, d.lat])[1])
+        .attr("cx", d => projection([d.targetLongitude, d.targetLatitude])[0]+250) // Use the projection to convert lat/long to x/y
+        .attr("cy", d => projection([d.targetLongitude, d.targetLatitude])[1])
         .attr("r", 5) // Adjust the radius as needed
-        .attr("fill", "red");
+        .attr("fill", pointColor);
+    });
+}
 
-            
 
-})
+function drawMapNormalChart(data1,data2){
+
+    console.log(data1,data2)
+
+    const svg = d3.select("#myMap svg");
+    const width = vw(43.5);
+    const height = vh(50);
+
+    svg.attr("width", width)
+   .attr("height", height);
+
+   const projection = d3.geoNaturalEarth1()
+   .scale(width / 1.45 / Math.PI)
+   .translate([width / 2, height / 2]);
+
+   d3.json("https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/world.geojson").then( function(worldData) {
+
+    // Draw the map
+    svg.append("g")
+    .selectAll("path")
+    .data(worldData.features)
+    .join("path")
+    .attr("fill", "#c0c1c2")
+    .attr("d", d3.geoPath().projection(projection))
+    .style("stroke", "#fff");
+
+    svg.append("g")
+    .selectAll("circle1")
+    .data(data1)
+    .attr("class","circle1")
+    .enter()
+    .append("circle")
+    .attr("cx", d => projection([d.targetLongitude, d.targetLatitude])[0]+250)
+    .attr("cy", d => projection([d.targetLongitude, d.targetLatitude])[1])
+    .attr("r", 5) // Set your desired radius
+    .attr("fill", "#704F4F");
+
+    // Draw circles for data2
+    svg.append("g")
+    .selectAll("circle2")
+    .attr("class","circle2")
+    .data(data2)
+    .enter()
+    .append("circle")
+    .attr("cx", d => projection([d.targetLongitude, d.targetLatitude])[0]+240)
+    .attr("cy", d => projection([d.targetLongitude, d.targetLatitude])[1])
+    .attr("r", 5) // Set your desired radius
+    .attr("fill", "#F1A661");
+        
+        
+        
+        })
+
 
 
 }
