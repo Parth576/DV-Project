166688bfa45e075e01677f281041eac2020843c1
thor <yadvenn@gmail.com>
2023-11-27 21:52:35 -0700

    fix iso week

--- js/utils.js
+++ js/utils.js
@@ -1,5 +1,9 @@
 export function getISOWeekNumber(date) {
-    const startDate = new Date(date.getFullYear(), 0, 1);
+    const startDate = new Date('2025-01-01T00:00:00');
     const days = Math.floor((date - startDate) / (24 * 60 * 60 * 1000));
-    return Math.floor(days / 7);
+    const x = Math.floor(days / 7);
+    if ([0, 1, 2].includes(date.getDay())) {
+        return x + 1
+    }
+    return x
 }
