cf25546ea4b7b25f5c18380011e53bff62a39b96
vishwesh <vishwesh103@gmail.com>
2023-11-30 10:35:46 -0700

    heatmap changes

--- js/heatmap.js
+++ js/heatmap.js
@@ -16,14 +16,22 @@ const widthWithMargin = vw(30), heightWithMargin = vh(25), margin = {top: vh(2),
     height = heightWithMargin - margin.top - margin.bottom;
 
 function drawGraph(filteredData, div_name, link_color) {
-    d3.select(`${div_name}`).selectAll("*").remove();
-    const svg = d3.select(div_name)
+    // d3.select(`${div_name}`).selectAll("*").remove();
+    let svg;
+    if (d3.select(`${div_name} svg g.heat-g`).empty()) {
+        svg = d3.select(div_name)
         .append("svg")
         .attr("width", widthWithMargin)
         .attr("height", heightWithMargin)
         .append("g")
+        .attr("class", "heat-g")
         .attr("transform",
             "translate(" + margin.left + "," + margin.top + ")");
+        
+    }
+    else {
+        svg = d3.select(`${div_name} svg g.heat-g`);
+    }
 
 
     const myGroups = Array.from(new Set(filteredData.map(d => getISOWeekNumber(d['time'])))).sort((a, b) => a - b)
@@ -36,29 +44,44 @@ function drawGraph(filteredData, div_name, link_color) {
 }
 
 function drawHeatmapInSvg(svg, myGroups, myVars, data, div_name, link_color) {
-    d3.select("#myMap svg").selectAll("*").remove();
 
     const months = [
         'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
         'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'
     ];
+    console.log(myGroups);
+    console.log(data);
     const x = d3.scaleBand()
         .range([0, width])
-        .domain(myGroups)
+        .domain(Array.from({ length: 52 }, (_, index) => index + 1))
         .padding(0.01);
 
 
     const xAxis = d3.scaleBand().range([0, width]).domain(months);
+    if (svg.select("g.xaxis").empty()) {
     svg.append("g")
+        .attr("class", "xaxis")
         .attr("transform", `translate(0, ${height})`)
         .call(d3.axisBottom(xAxis))
+    } else {
+        svg.select("g.xaxis")
+        .call(d3.axisBottom(xAxis))
+    }
 
     const y = d3.scaleBand()
         .range([height, 0])
         .domain(myVars)
         .padding(0.01);
+
+    if (svg.select("g.yaxis").empty()) {
     svg.append("g")
+    .attr("class", "yaxis")
         .call(d3.axisLeft(y));
+    }
+    else {
+        svg.select("g.yaxis")
+        .call(d3.axisLeft(y))
+    }
 
         
 
@@ -67,7 +90,9 @@ function drawHeatmapInSvg(svg, myGroups, myVars, data, div_name, link_color) {
         .range([link_color[0],link_color[1]])
         .domain([1, d3.max(data, (d) => d.value)])
 
-    const tooltip = d3.select(div_name)
+    let tooltip;
+    if (d3.select(".heatmap-tooltip").empty()) {
+        tooltip = d3.select(div_name)
         .append("div")
         .style("opacity", 0)
         .attr("class", "heatmap-tooltip")
@@ -76,6 +101,16 @@ function drawHeatmapInSvg(svg, myGroups, myVars, data, div_name, link_color) {
         .style("border-width", "2px")
         .style("border-radius", "5px")
         .style("padding", "5px")
+    } else {
+        tooltip = d3.select(".heatmap-tooltip")
+        .style("opacity", 0)
+        .attr("class", "heatmap-tooltip")
+        .style("background-color", "white")
+        .style("border", "solid")
+        .style("border-width", "2px")
+        .style("border-radius", "5px")
+        .style("padding", "5px")
+    }
 
     const mouseover = function () {
         tooltip
@@ -98,11 +133,23 @@ function drawHeatmapInSvg(svg, myGroups, myVars, data, div_name, link_color) {
             .style("opacity", 0.8)
     }
 
-    svg.selectAll()
+    svg.selectAll(".cell")
         .data(data, function (d) {
             return d.group + ':' + d.variable;
         })
         .join("rect")
+        .attr("class", "cell")
+        .on("mouseover", mouseover)
+        .on("mousemove", mousemove)
+        .on("mouseleave", mouseleave)
+        .on("click", (e, d) => {
+            console.log(e, d)
+            appState.applyFilters({
+                'startTime': d.date.setHours(0, 0, 0, 0),
+                'endTime': d.date.setHours(23, 59, 59, 999),
+            }, "heatmap");
+        })
+        .transition().duration(500)
         .attr("x", function (d) {
             return x(d.group)
         })
@@ -119,16 +166,6 @@ function drawHeatmapInSvg(svg, myGroups, myVars, data, div_name, link_color) {
         .style("stroke-width", 4)
         .style("stroke", "none")
         .style("opacity", 1)
-        .on("mouseover", mouseover)
-        .on("mousemove", mousemove)
-        .on("mouseleave", mouseleave)
-        .on("click", (e, d) => {
-            console.log(e, d)
-            appState.applyFilters({
-                'startTime': d.date.setHours(0, 0, 0, 0),
-                'endTime': d.date.setHours(23, 59, 59, 999),
-            }, "heatmap");
-        })
 }
 
 function computeHeatmapData(data, myVars) {
